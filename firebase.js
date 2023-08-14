ece55d9ddc068f0462b02a1e3662d41204c7e8c2
susanzh4ng <112526560+susanzh4ng@users.noreply.github.com>
2023-08-14 17:21:20 -0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -2,12 +2,7 @@ import { initializeApp } from 'firebase/app'
 import {getAuth} from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDLVAyYcFjIosge-YMDzQbiWCfYV5kA6Vc",
-  authDomain: "fir-auth-b5e9a.firebaseapp.com",
-  projectId: "fir-auth-b5e9a",
-  storageBucket: "fir-auth-b5e9a.appspot.com",
-  messagingSenderId: "279613250045",
-  appId: "1:279613250045:web:f3d2d1aa9386808eb8a194"
+  // ...
 };
 
 export const FIREBASE_APP = initializeApp(firebaseConfig);
