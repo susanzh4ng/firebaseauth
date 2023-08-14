cdf170b4632896ee423d9afb4a5457b786deff6b
susanzh4ng <112526560+susanzh4ng@users.noreply.github.com>
2023-08-14 17:21:45 -0500

    Update App.js

--- App.js
+++ App.js
@@ -7,12 +7,7 @@ import HomeScreen from './screens/HomeScreen';
 import { initializeApp } from 'firebase/app'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDLVAyYcFjIosge-YMDzQbiWCfYV5kA6Vc",
-  authDomain: "fir-auth-b5e9a.firebaseapp.com",
-  projectId: "fir-auth-b5e9a",
-  storageBucket: "fir-auth-b5e9a.appspot.com",
-  messagingSenderId: "279613250045",
-  appId: "1:279613250045:web:f3d2d1aa9386808eb8a194"
+  // ...
 };
 
 export const FIREBASE_APP = initializeApp(firebaseConfig);
